bd4b5a26e7164559cb57593df2f7b95c0c89844f
Franck SORO <francksoro@Air-de-Franck.lan>
2024-03-31 04:30:06 +0200

    code

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "name" {
     Name = "Test25"
   }
 }
+
+resource "aws_instance" "name2" {
+  instance_type = var.instance_type
+  ami = data.aws_ami.amazon-linux.id
+
+  tags = {
+    Name = "Test225"
+  }
+}
