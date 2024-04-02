c8545c2656fd6c935c60ae4049a9f239def6c30c
Franck SORO <francksoro@Air-de-Franck.lan>
2024-04-03 01:05:56 +0200

    webhook push test

--- ec2.tf
+++ ec2.tf
@@ -6,12 +6,3 @@ resource "aws_instance" "name" {
     Name = "Test25"
   }
 }
-
-resource "aws_instance" "name2" {
-  instance_type = var.instance_type
-  ami = data.aws_ami.amazon-linux.id
-
-  tags = {
-    Name = "Test225"
-  }
-}
